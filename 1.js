d6fe2f7783d6f944b3393b9300f3024a39c14ad4
QianxueSno <qzhao01@risd.edu>
2021-03-24 19:08:23 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
   $('.image-link').magnificPopup({type:'image'});
 });
+
+var $to = $("<div/>",{id:"to"}).on("click", function(){
+    $(this).removeClass("on");
+}).appendTo("body");
+$('img[data-darkbox]').css({cursor:"pointer"}).on("click",function(){
+    var o=this.getBoundingClientRect();
+    $about.css({
+        transition:"0s",
+        backgroundImage:"url("+this.src+")",
+        left:o.left, top:o.top,
+        height:this.height, width:this.width
+    });
+    setTimeout(function(){
+        $about.css({transition:".8s"}).addClass("on");
+    },5);
+});
